fc784e115e2cad3e635de41b72c5342dc15ea4ea
midrra <el.theearth@gmail.com>
2025-09-27 14:33:28 +0300

    some changes in all

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -7,22 +7,26 @@ export const clerkWebhooks = async (req, res) => {
   try {
     if (!process.env.CLERK_WEBHOOK_SECRET) {
       console.error("❌ Missing CLERK_WEBHOOK_SECRET in environment variables");
+      return res
+        .status(500)
+        .json({ success: false, message: "Server configuration error" });
     }
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-    const payload = req.body;
-    const evt = await whook.verify(JSON.stringify(req.body), {
+
+    const payload = req.body.toString("utf8");
+    const evt = whook.verify(payload, {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
     });
 
-    const { data, type } = req.body;
-    console.log("Webhook verified:", data);
+    const { data, type } = evt;
+    console.log("Webhook verified:", type, data.id);
 
     switch (type) {
       case "user.created": {
         const userData = {
-          _id: data._id,
+          _id: data.id,
           email: data.email_addresses[0].email_address,
           name: data.first_name + "" + data.last_name,
           imageUrl: data.image_url,
@@ -37,13 +41,13 @@ export const clerkWebhooks = async (req, res) => {
           name: data.first_name + "" + data.last_name,
           imageUrl: data.image_url,
         };
-        await User.findByIdAndUpdate(data._id, userData);
+        await User.findByIdAndUpdate(data.id, userData);
         res.json({});
         break;
       }
 
       case "user.deleted": {
-        await User.findByIdAndDelete(data._id);
+        await User.findByIdAndDelete(data.id);
         res.json({});
         break;
       }
